ef203fe0b1fd17d39570cfe5f9e5f7aab47cfe72
Richard Rozario A <richardrozario2000@gmail.com>
2023-06-26 14:57:50 +0530

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const userroutes = require('./routes/user.routes');
-const bodyparser = require('body-parser');
-
-const app = express();
-app.use(bodyparser.json());
-app.use(userroutes);
-
-module.exports = app;
